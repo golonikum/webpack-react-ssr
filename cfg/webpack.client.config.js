b0ecca842748cf81e23f0cc97e42d85a57a7abde
Nikita Golov <goloniko@gmail.com>
2022-09-13 18:16:33 +0300

    Update webpack.client.config.js

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -1,36 +1,44 @@
-const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
 
 const NODE_ENV = process.env.NODE_ENV;
-const IS_DEV = NODE_ENV === 'development';
+const IS_DEV = NODE_ENV === "development";
 
 function setupDevtool() {
-    return IS_DEV ? 'eval' : false;
+  return IS_DEV ? "eval" : false;
 }
 
 module.exports = {
-    mode: NODE_ENV ? NODE_ENV : 'development',
-    entry: path.resolve(__dirname, '../src/client/index.jsx'),
-    output: {
-        path: path.resolve(__dirname, '../dist/client'),
-        filename: 'client.js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-    },
-    module: {
-        rules: [{
-            test: /\.[tj]sx?$/,
-            use: ['ts-loader'],
-        }],
-    },
-    // plugins: [
-    //     new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html')})
-    // ],
-    // devServer: {
-    //     port: 3000,
-    //     open: true,
-    //     hot: IS_DEV,
-    // },
-    devtool: setupDevtool(),
+  mode: NODE_ENV ? NODE_ENV : "development",
+  entry: path.resolve(__dirname, "../src/client/index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "../dist/client"),
+    filename: "client.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                mode: "local",
+                localIdentName: "[name]__[local]--[hash:base64:5]",
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devtool: setupDevtool(),
 };
